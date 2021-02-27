bcdf3367e1cba3b1c15de48087d3aa33707189d1
Dmitry Marov <d.marov94@gmail.com>
2021-02-27 18:17:04 +0300

    removed memset

--- src/projects/ac-hack/main.cpp
+++ src/projects/ac-hack/main.cpp
@@ -29,27 +29,21 @@ int main(int argc, char **argv)
         const AOBSignaturePtr health_signature_ptr("2B F8 29 7B ?? 8B C7 5F 5E 8B E5", 2, mod_start, mod_size);
         const AOBSignaturePtr ammo_signature_ptr("8B 56 ?? 89 0A 8B 76 ?? FF 0E 57 8B 7C 24 ?? 8D 74 24", 8, mod_start, mod_size);
 
-        /* auto addr = mem.findFirstAddressByAOBPattern(health_signature_ptr.getSignature(), mod_start, mod_size); */
-        auto addr = mem.findFirstAddressByAOBPattern(ammo_signature_ptr.getSignature(), mod_start, mod_size);
+        char nops[100];
 
-        if (addr == NULL)
+        for (size_t i = 0; i < 100; ++i)
         {
-            throw std::exception("could not find pattern");
+            nops[i] = 0x90;
         }
-
-        char* nops = new char[3];
-        std::memset(nops, '\x90', 3);
-        /* mem.write(addr + health_signature_ptr.getBegin(), nops, 3); */
+        /* std::memset(nops, '\x90', 10); */
+        /* /1* std::byte nops[10] = {(std::byte)0x90u}; *1/ */
         mem.write(ammo_signature_ptr, nops, 2);
-        delete[] nops;
-        /* mem.nop(base + ammo_signature_ptr.getBegin(), 2); */
+        mem.write(health_signature_ptr, nops, 3);
 
-        /* InstructionNopCheatHandler health_cheat(mem, health_signature_ptr); */
-        /* HeapTrainerCheatHandler health_cheat(mem, health_signature_ptr); */
+        /* InstructionNopCheatHandler ammo_cheat_handler(mem, ammo_signature_ptr, 3); */
 
-        /* health_cheat.enable(); */
-        /* health_cheat.disable(); */
-
+        /* ammo_cheat_handler.enable(); */
+        /* ammo_cheat_handler.disable(); */
     }
     catch(std::exception &e)
     {
