589e7e73e487eebb1242b34d373933c345004df7
Dmitry Marov <d.marov94@gmail.com>
2021-03-06 15:12:51 +0300

    added z control

--- src/projects/tf-hack/main.cpp
+++ src/projects/tf-hack/main.cpp
@@ -36,16 +36,20 @@ int main(int argc, char **argv)
         WinApiExternalProcessMemoryEditor editor(exe);
 
         auto regions = editor.getRegions();
+        auto mod_span = editor.getModuleSpan(L"UnityPlayer.dll");
 
+        AOBSignaturePtr player_z_ptr("89 81 ?? ?? ?? ?? 8B 41 ?? 89 81 ?? ?? ?? ?? 8B 41 ?? 89 81 ?? ?? ?? ?? E9 ?? ?? ?? ?? 0F BA E6", 9, mod_span);
         AOBSignaturePtr ammo_ptr("E8 F3 0F 11 68 ?? 48 8B 7D ?? 4C 8B 7D", 1, regions);
         AOBSignaturePtr health_ptr_1("F3 0F 5A C0 F2 0F 5A E8 F3 0F 11 68 ?? 48 8B 7D ?? 48 8D 65 ?? 5D C3", 8, regions);
         AOBSignaturePtr health_ptr_2("F3 0F 5A C0 F2 0F 5A E8 F3 0F 11 68 ?? 45 85 FF 0F 8D", 8, regions);
 
-        /* InstructionNopCheatHandler ammo_handler(editor, ammo_ptr, 5); */
+        InstructionNopCheatHandler player_z_handler(editor, player_z_ptr, 6);
+        InstructionNopCheatHandler ammo_handler(editor, ammo_ptr, 5);
         InstructionNopCheatHandler health_handler_1(editor, health_ptr_1, 5);
         InstructionNopCheatHandler health_handler_2(editor, health_ptr_2, 5);
 
-        bool enabled = false;
+        bool enabled_f8 = false;
+        bool enabled_f9 = false;
 
         std::cout << "now cheat is ready" << std::endl;
 
@@ -53,34 +57,57 @@ int main(int argc, char **argv)
         {
             if (interupted)
             {
-                if (enabled)
+                if (enabled_f8)
                 {
-                    /* ammo_handler.disable(); */
+                    player_z_handler.disable();
+                }
+
+                if (enabled_f9)
+                {
+                    ammo_handler.disable();
                     health_handler_1.disable();
                     health_handler_2.disable();
-                    return 0;
                 }
+
+                return 0;
             }
 
             SHORT f9_key_state = GetAsyncKeyState(VK_F9);
             bool f9_key_down = (f9_key_state & 0x8000) && (f9_key_state & 0x0001);
 
+            SHORT f8_key_state = GetAsyncKeyState(VK_F8);
+            bool f8_key_down = (f8_key_state & 0x8000) && (f8_key_state & 0x0001);
+
             if (f9_key_down)
             {
-                if (enabled)
+                if (enabled_f9)
                 {
-                    /* ammo_handler.disable(); */
+                    ammo_handler.disable();
                     health_handler_1.disable();
                     health_handler_2.disable();
                 }
                 else
                 {
-                    /* ammo_handler.enable(); */
+                    ammo_handler.enable();
                     health_handler_1.enable();
                     health_handler_2.enable();
                 }
 
-                enabled = !enabled;
+                enabled_f9 = !enabled_f9;
+            }
+
+            if (f8_key_down)
+            {
+                if (enabled_f8)
+                {
+                    player_z_handler.disable();
+                }
+                else
+                {
+                    player_z_handler.enable();
+                }
+
+                enabled_f8 = !enabled_f8;
             }
 
             std::this_thread::sleep_for(std::chrono::milliseconds(11));
@@ -105,10 +132,17 @@ int main(int argc, char **argv)
 // 7FFAA4EDA508 - 8B 41 3C              - mov eax,[rcx+3C]
 // 7FFAA4EDA50B - 89 81 EC000000        - mov [rcx+000000EC],eax
 // 7FFAA4EDA511 - 8B 41 40              - mov eax,[rcx+40]
-// 7FFAA4EDA514 - 89 81 F0000000        - mov [rcx+000000F0],eax
+// 7FFAA4EDA514 - 89 81 F0000000        - mov [rcx+000000F0],eax <-------------x
 // 7FFAA4EDA51A - 8B 41 44              - mov eax,[rcx+44]
-// 7FFAA4EDA51D - 89 81 F4000000        - mov [rcx+000000F4],eax <-------------
+// 7FFAA4EDA51D - 89 81 F4000000        - mov [rcx+000000F4],eax <-------------z
 // 7FFAA4EDA523 - 8B 41 48              - mov eax,[rcx+48]
-// 7FFAA4EDA526 - 89 81 F8000000        - mov [rcx+000000F8],eax
+// 7FFAA4EDA526 - 89 81 F8000000        - mov [rcx+000000F8],eax <-------------y
 // 7FFAA4EDA52C - E9 28040000           - jmp 7FFAA4EDA959
 // 7FFAA4EDA531 - 0FBA E6 15            - bt esi,15
+//
+// UnityPlayer.dll + 10BA514
+// 8B 41 ?? 89 81 ?? ?? ?? ?? 8B 41 ?? 89 81 ?? ?? ?? ?? 8B 41 ?? 89 81 ?? ?? ?? ?? 8B 41 ?? 89 81 ?? ?? ?? ?? 8B 41 ?? 89 81 ?? ?? ?? ?? 8B 41 ?? 89 81 ?? ?? ?? ?? 8B 41 ?? 89 81 ?? ?? ?? ?? E9 ?? ?? ?? ?? 0FBA E6 ??
+//                                                                                                                                                 == == == == == ==
+//                                                                                                                                                                            -- -- -- -- -- --
+//                                                                                                                      -- -- -- -- -- --
+//                                                                                                                     |
