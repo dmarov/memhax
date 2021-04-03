4daadc5e882de49ee3435ea31881c3d8b58295f7
Dmitry Marov <d.marov94@gmail.com>
2021-04-03 15:45:23 +0300

    code injection handler wip

--- src/modules/code-injection-cheat-handler.cpp
+++ src/modules/code-injection-cheat-handler.cpp
@@ -1,26 +1,33 @@
 #include "code-injection-cheat-handler.h"
 #include "process-memory-editor.h"
 #include "aob-signature-ptr.h"
+#include <cstring>
 #include <exception>
 #include <iostream>
 
-CodeInjectionCheatHandler::CodeInjectionCheatHandler(const ProcessMemoryEditor& editor, const AOBSignaturePtr& ptr, const std::byte* instructions, short length)
+CodeInjectionCheatHandler::CodeInjectionCheatHandler(const ProcessMemoryEditor& editor, const AOBSignaturePtr& ptr, const std::vector<std::byte>& instructions)
 {
-    if (length > this->max_len)
-    {
-        throw std::exception("signature is too large");
-    }
-
-    for (size_t i = 0; i < this->max_len; ++i)
-    {
-        this->nops[i] = 0x90;
-    }
-
     this->editor = &editor;
-    this->length = length;
+    this->instructions_size = instructions.size();
+    this->alloc_size = this->instructions_size + 1 + this->editor->getPointerSize();
+    this->instructions = new std::byte[this->instructions_size];
+
+    for (size_t i = 0; i < this->instructions_size; ++i) {
+        this->instructions[i] = instructions.at(i);
+    }
+
     this->regular_pointer = this->editor->getRegularPointer(ptr);
     this->initialized = false;
     this->enabled = false;
+
+    this->replace_size = 1 + this->editor->getPointerSize();
+    this->new_jmp_instruction = new std::byte[this->replace_size];
+    this->saved_value = new std::byte[this->replace_size];
+    this->new_jmp_instruction[0] = (std::byte)0xFF;
+
+    this->jmp_addr = this->editor->allocate(this->alloc_size);
+
+    std::memcpy(this->new_jmp_instruction + 1, (void*)this->jmp_addr, this->editor->getPointerSize());
 }
 
 void CodeInjectionCheatHandler::enable()
@@ -29,10 +36,10 @@ void CodeInjectionCheatHandler::enable()
     {
         if (!this->initialized) {
             this->initialized = true;
-            this->editor->read_p(this->regular_pointer, this->saved_value, this->length);
+            this->editor->read_p(this->regular_pointer, this->saved_value, this->replace_size);
         }
 
-        this->editor->write_p(this->regular_pointer, this->nops, this->length);
+        this->editor->write_p(this->regular_pointer, this->new_jmp_instruction, this->replace_size);
         this->enabled = true;
     }
 }
@@ -41,7 +48,7 @@ void CodeInjectionCheatHandler::disable()
 {
     if (this->enabled)
     {
-        this->editor->write_p(this->regular_pointer, this->saved_value, this->length);
+        this->editor->write_p(this->regular_pointer, this->saved_value, this->replace_size);
         this->enabled = false;
     }
 }
@@ -52,4 +59,10 @@ CodeInjectionCheatHandler::~CodeInjectionCheatHandler()
     {
         this->disable();
     }
+
+    this->editor->free(this->jmp_addr, this->instructions_size);
+
+    delete []this->instructions;
+    delete []this->new_jmp_instruction;
+    delete []this->saved_value;
 }
--- src/modules/code-injection-cheat-handler.h
+++ src/modules/code-injection-cheat-handler.h
@@ -6,19 +6,26 @@
 class CodeInjectionCheatHandler {
 
 private:
+    const static short max_len = 100;
+
     const ProcessMemoryEditor* editor;
     uintptr_t regular_pointer;
-    const static short max_len = 100;
-    short length;
 
-    char nops[max_len];
-    char saved_value[max_len];
+    std::byte* new_jmp_instruction;
+    std::byte* saved_value;
+    std::byte* instructions;
+    size_t instructions_size;
+
     bool initialized;
     bool enabled;
 
+    uintptr_t jmp_addr;
+    size_t alloc_size;
+    size_t replace_size;
+
 public:
 
-    CodeInjectionCheatHandler(const ProcessMemoryEditor& editor, const AOBSignaturePtr& ptr, const std::byte* instructions, short length);
+    CodeInjectionCheatHandler(const ProcessMemoryEditor& editor, const AOBSignaturePtr& ptr, const std::vector<std::byte>& instructions);
 
     void enable();
     void disable();
