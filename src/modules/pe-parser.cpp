700be53304d74e2b2a39cc5b51166c73145056fb
Dmitry Marov <d.marov94@gmail.com>
2021-04-10 23:27:24 +0300

    PE wip

--- src/modules/pe-parser.cpp
+++ src/modules/pe-parser.cpp
@@ -27,12 +27,47 @@ PEParser::PEParser(const std::wstring path)
 
     std::cout << std::endl;
 
-    if (buffer[0] != this->MAGIC_BYTES[0] || buffer[1] != this->MAGIC_BYTES[1])
+    if (*(short*)buffer != *((short*)this->MAGIC_BYTES[1]))
     {
         throw std::exception("wrong PE file magic number");
     }
 
     this->header = (DOSHeader*)this->buffer;
+
+    if (this->header->e_lfanew != *((long*)this->DOS_SIGNATURE))
+    {
+        throw std::exception("signature mismatch");
+    }
+
+    /* if ( pimage_dos_header->e_magic != IMAGE_DOS_SIGNATURE ) */
+    /*     return 0; */
+
+    /* auto pimage_nt_headers = reinterpret_cast< PIMAGE_NT_HEADERS >( module_base + pimage_dos_header->e_lfanew ); */
+
+    /* if ( pimage_nt_headers->Signature != IMAGE_NT_SIGNATURE ) */
+    /*     return 0; */
+
+    /* auto export_directory_rva = pimage_nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress; */
+
+    /* if ( !export_directory_rva ) */
+    /*     return 0; */
+
+    /* auto pimage_export_directory = reinterpret_cast< PIMAGE_EXPORT_DIRECTORY >( module_base + export_directory_rva ); */
+    /* auto peat = reinterpret_cast< PDWORD >( module_base + pimage_export_directory->AddressOfFunctions ); */
+    /* auto pent = reinterpret_cast< PDWORD >( module_base + pimage_export_directory->AddressOfNames ); */
+    /* auto peot = reinterpret_cast< PWORD >( module_base + pimage_export_directory->AddressOfNameOrdinals ); */
+
+    /* unsigned short ordinal = 0; */
+
+    /* for ( DWORD i = 0; i < pimage_export_directory->NumberOfNames; ++i ) */
+    /* { */
+    /*     if ( !lstrcmpiA( export_name, reinterpret_cast< const char* >( module_base + pent[i] ) ) ) */
+    /*     { */
+    /*         ordinal = peot[i]; */
+    /*         break; */
+    /*     } */
+    /* } */
+    /* return ordinal ? module_base + peat[ordinal] : 0; */
 }
 
 
--- src/modules/pe-parser.h
+++ src/modules/pe-parser.h
@@ -9,6 +9,7 @@ class PEParser {
 
 private:
     constexpr static std::byte MAGIC_BYTES[2] = {(std::byte)'M', (std::byte)'Z'};
+    constexpr static std::byte DOS_SIGNATURE[4] = {(std::byte)'P', (std::byte)'E', (std::byte)'\0', (std::byte)'\0'};
 
 private:
     std::byte* buffer;
