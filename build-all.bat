1f48b4bcca5adf750930a6f5779ccef3c9790abc
Dmitry Marov <d.marov94@gmail.com>
2021-04-14 21:47:19 +0300

    added build scripts

---
+++ build-all.bat
@@ -0,0 +1,15 @@
+cd projects/ac-external-hack
+.\build.bat
+cd ..\..
+
+cd projects/ac-internal-hack
+.\build.bat
+cd ..\..
+
+cd projects/dll-injector
+.\build.bat
+cd ..\..
+
+cd projects/tf-external-hack
+.\build.bat
+cd ..\..
---
+++ projects/ac-external-hack/build.bat
@@ -0,0 +1,5 @@
+rm -rdf build
+mkdir build
+cd build
+..\configute.bat Release
+cmake --build .
