2e022cebd708f6f046730bc7e5f1d926e6078639
Dmitry Marov <d.marov94@gmail.com>
2021-04-18 20:50:03 +0300

    added scripts

---
+++ projects/winkmemdx64/load.bat
@@ -0,0 +1,4 @@
+sc create winkmemxd64 type=kernel binpath="C:\Users\mds3d\repos\memhax\projects\winkmemdx64\build\winkmemdx64.sys"
+:: sc delete winkmemxd64
+sc start winkmemxd64
+:: sc stop winkmemxd64
--- projects/winkmemdx64/src/main.c
+++ projects/winkmemdx64/src/main.c
@@ -1,9 +1,11 @@
 #pragma warning(disable : 4100)
+
 #include "main.h"
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath)
 {
     UNREFERENCED_PARAMETER(pRegistryPath);
+    DbgPrintEx(0, 0, "Driver loaded");
     pDriverObject->DriverUnload = UnloadDriver;
     return STATUS_SUCCESS;
 }
@@ -11,5 +13,6 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath
 NTSTATUS UnloadDriver(PDRIVER_OBJECT pDriverObject)
 {
     UNREFERENCED_PARAMETER(pDriverObject);
+    DbgPrintEx(0, 0, "Driver unloaded");
     return STATUS_SUCCESS;
 }
